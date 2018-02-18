486b416a790dbcbda8e35717e10a6c63a4236948
KrishCR97 <veerakishon.mucherla@gmail.com>
2018-02-17 18:59:30 -0600

    Added routes for the application.

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ let http = require('http');
 let fs = require('fs');
 http.createServer((req,res)=>{
 //MIME type
-res.writeHead(200,{'content-Type' : 'text/html'});
-fs.createReadStream(__dirname + '/index.html').pipe(res);
+res.writeHead(200,{'content-Type' : 'application/json'});
+if(req.url == "/api"){
+    var obj = {
+        nickName : "Kishon",
+        lastName : "Mucherla"
+    };
+    res.end(JSON.stringify(obj));
+}
+else if(req.url == "/"){
+    res.writeHead(200,{'content-Type' : 'text/html'});
+    var html = fs.readFileSync(__dirname + "/index.html");
+    res.end(html);
+
+}
+res.writeHead(404);
+res.end(404+"");
+console.log(req);
+// fs.createReadStream(__dirname + '/index.html').pipe(res);
 // html = html.replace('{msg}',"Kishon");
 // res.end(html);
 }).listen(4000,'127.0.0.1');
