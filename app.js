9be157c353ea34d09f6f66318a492d1e7e9d5705
KrishCR97 <veerakishon.mucherla@gmail.com>
2018-02-17 18:36:10 -0600

    Added chunks of code.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let fs = require('fs');
 http.createServer((req,res)=>{
 //MIME type
 res.writeHead(200,{'content-Type' : 'text/html'});
-var html = fs.readFileSync(__dirname + '/index.html');
-res.end(html);
+fs.createReadStream(__dirname + '/index.html').pipe(res);
+// html = html.replace('{msg}',"Kishon");
+// res.end(html);
 }).listen(4000,'127.0.0.1');
